243be7a1b8e101d126ccc342a890edacc725e672
Filip Bontin <filip@no-reply.example.org>
2026-04-21 10:51:54 +0200

    Add basic setup showing bug and square, controlled using buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
+import { useState } from 'react';
+import { Canvas } from '@react-three/fiber'
+import Bug from './Bug.tsx';
 import './App.css'
 
 function App() {
-  return <h1>TODO</h1>
+  const [size, setSize] = useState(1);
+  const [showBug, setShowBug] = useState(false);
+
+  return (
+    <>
+      <div id="canvas-container">
+        <Canvas>
+          <ambientLight intensity={0.1} />
+          <directionalLight color="white" position={[0, 0, 5]} />
+          <mesh>
+            <boxGeometry args={[size, size, size]} />
+            <meshStandardMaterial />
+          </mesh>
+          {showBug && <Bug size={size} />}
+        </Canvas>
+      </div>
+      <div className='buttons'>
+        <button onClick={() => setSize(n => n - 1)}>Size -1</button>
+        <button onClick={() => setSize(n => n + 1)}>Size +1</button>
+        <button onClick={() => setShowBug(current => !current)}>Toggle bug</button>
+      </div>
+    </>
+  )
 }
 
 export default App
